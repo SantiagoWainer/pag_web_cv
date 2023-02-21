4ffecc6b1d28546b0539a89fa718ad666e56e8f7
Santiago Wainer <santiwainer@gmail.com>
2023-02-21 17:48:08 -0300

    Corrección hipervínculo de cursos

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 // Creando filtrado de busqueda
 
 function busqueda(){
+    // Tomo el valor
     let inputValue = document.getElementById("inputSearch").value;
+
+    // La paso a minuscula
     let palabra = inputValue.toLowerCase();
+
+    // Genero la condición
+
+
+     
     if (palabra == 'language' || palabra == 'idioma' || palabra == 'idiomas' || palabra == 'languages' || palabra == 'estudios'|| palabra == 'estudio'|| palabra == 'studies'|| palabra == 'degree') {
-        location.href = "\html\studies.html";
+        window.location.href = 'html\studies.html';
     }
     if (palabra == 'curriculum' || palabra == 'cv') {
         location.href = "\html\myself.html";
@@ -14,7 +22,11 @@ function busqueda(){
     } 
     if (palabra == 'contacto' || palabra == 'contact') {
         location.href = "\html\contact.html";
-    }   
+    }
+    
+    // while (palabra == 'language' || palabra == 'idioma' || palabra == 'idiomas' || palabra == 'languages' || palabra == 'estudios'|| palabra == 'estudio'|| palabra == 'studies'|| palabra == 'degree') {
+    //     window.location.href = 'https://santiwainer.wixsite.com/travellingdotcom';
+    // }   
 }
 
 document.getElementById('botonBusqueda').addEventListener('click', busqueda);
